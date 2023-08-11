399db86c2f964f40f58b0e8c3274fb0c8bcaaf91
40iq <maksPTD6@mail.ru>
2023-08-11 19:53:14 +0300

    feat: add brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function check(str, bracketsConfig) {
+ module.exports = function check(str, bracketsConfig) {
   // your solution
+  let bracketsConfigOpenClosed = [];
+  bracketsConfig.forEach((item) => {
+    bracketsConfigOpenClosed.push(`${item[0]}${item[1]}`);
+  })
+
+  for (let i = 0; i < str.length; i++) {
+    let eachTwoSymbleOfStr = str.slice(i, i + 2);
+    
+    if (str.length === 2) {eachTwoSymbleOfStr = str}
+    
+    let isEqual = 0;
+    
+    bracketsConfigOpenClosed.map((item) => {
+      if (item === eachTwoSymbleOfStr) {isEqual = true};
+    });
+
+    if (isEqual) {
+      if (str.length === 2) {
+        str = str.slice(0, 0)
+      } else {
+        str = str.slice(0, i) + str.slice(i + 2, str.length);
+      }
+      i = 0;
+    }  
+  }
+  if (str === '') {return true}
+  else {return false}
 }
